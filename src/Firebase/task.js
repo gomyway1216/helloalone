6da6074fbe1a60255d67ab9a02e8354559585ba4
gomyway1216 <uwyudai@gmail.com>
2022-09-01 17:50:34 -0700

    clean up

--- src/Firebase/task.js
+++ src/Firebase/task.js
@@ -51,29 +51,3 @@ export const getTasks = (userId) => {
       return list;
     });
 };
-
-
-// export const getTask = (userName, taskId) => {
-//   return getDbAccess().collection(userName).doc('task').collection('taskCollection').doc(taskId)
-//     .get().then(doc => {
-//       return { id: doc.id, value: doc.data()};
-//     });
-// };
-
-export const getDailyTask = async (userName) => {
-  const today = new Date();
-  today.setHours(0,0,0,0);
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
-  const date1 = firebase.firestore.Timestamp.fromDate(today);
-  const date2 = firebase.firestore.Timestamp.fromDate(tomorrow);
-  const response = [];
-  await getDbAccess().collection(userName).doc('task').collection('dailyCollection')
-    .where('timestamp', '>=', date1).where('timestamp', '<=', date2)
-    .get().then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        response.push({id: doc.id, value: doc.data()});
-      });
-    });
-  return response[0];
-};
